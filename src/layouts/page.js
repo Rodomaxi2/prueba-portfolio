b0d95f0f061b3f75005a00cf4b7a76037e8bba7a
Stackbit Code Editor <projects@stackbit.com>
2021-11-11 20:57:40 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -24,7 +24,7 @@ export default class Page extends React.Component {
                     </header>
                     {image && (
                         <div className="post-image">
-                            <img src={withPrefix(image)} alt={imageAlt} width="10%"/>
+                            <img src={withPrefix(image)} alt={imageAlt}/>
                         </div>
                     )}
                     {markdownContent && <div className="post-content inner-sm">{markdownify(markdownContent)}</div>}
